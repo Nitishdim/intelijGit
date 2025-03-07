6bb0b8def3c5c31a382c78346fd416d84b0f41c3
Nitishdim <154655754+Nitishdim@users.noreply.github.com>
2025-03-07 21:42:20 +0530

    Update A.java

--- src/main/java/in/nitishDimpal/A.java
+++ src/main/java/in/nitishDimpal/A.java
@@ -4,6 +4,6 @@ public class A {
   int x =100;
   public static void main(String[] args){
     if(x>50){
-      System.out.println("I love u");
+      System.out.println("I love u Dimpal");
     }
 }
