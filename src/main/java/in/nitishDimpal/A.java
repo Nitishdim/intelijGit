264bf358484fc313354a1ba6d073f56b3b7bcc1c
Nitishdim <154655754+Nitishdim@users.noreply.github.com>
2025-03-07 21:44:36 +0530

    Update A.java

--- src/main/java/in/nitishDimpal/A.java
+++ src/main/java/in/nitishDimpal/A.java
@@ -1,9 +1,10 @@
 package in.nitishDimpal;
 
 public class A {
-  int x =100;
+  int static x =100;
   public static void main(String[] args){
     if(x>50){
       System.out.println("I love u Dimpal");
     }
+}
 }
